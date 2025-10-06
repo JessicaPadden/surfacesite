c77f7287d3d58a3d414a78dc7e2159ebec7ac285
Jessica Padden <jessica.padden99@gmail.com>
2025-10-06 18:36:09 +0100

    astro.mjs move

--- .github/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config'
-
-export default defineConfig({
-  site: 'https://github.com/JessicaPadden/Jessicafinal',
-  base: '/Jessicafinal/',
-  output: "static"
-});
-
