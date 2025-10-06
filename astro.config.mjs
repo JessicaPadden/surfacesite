0c39ae072cab74685a455502b5b3a650a31c5c99
Jessica Padden <jessica.padden99@gmail.com>
2025-10-06 18:54:43 +0100

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://github.com/JessicaPadden/surfacesite',
+  site: 'https://github.com/JessicaPadden.github.io',
   base: '/surfacesite/',
   output: "static"
 });
